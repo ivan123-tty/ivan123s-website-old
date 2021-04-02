228e797634e07e74a892c1471b30decdc247884e
ivan123 <70339505+ivan123gm@users.noreply.github.com>
2021-04-02 23:57:19 +0300

    another small fix

--- js/images.js
+++ js/images.js
@@ -1,6 +1,6 @@
 function langRU() {;
 	/*translations itself*/
-	document.getElementById("main").innerHTML = "Творения ivan123"
+	document.getElementById("creations").innerHTML = "Творения ivan123"
 	document.getElementById("nothing").innerHTML = "Здесь еще ничего нет :("
 	/*changing country flag and onclick function*/
 	document.getElementById("lang").src = "country-flags/uk.svg"
@@ -8,7 +8,7 @@ function langRU() {;
 }
 function langEN() {;
 	/*translations itself*/
-	document.getElementById("main").innerHTML = "ivan123's creations"
+	document.getElementById("creations").innerHTML = "ivan123's creations"
 	document.getElementById("nothing").innerHTML = "Nothing here yet :("
 	/*changing country flag and onclick function*/
 	document.getElementById("lang").src = "country-flags/ru.svg"
