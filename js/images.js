9089306aaade110d750ef2d6eccec89672af8b72
ivan123 <70339505+ivan123gm@users.noreply.github.com>
2021-04-02 23:37:36 +0300

    first version of new images page

--- js/images.js
+++ js/images.js
@@ -1,17 +1,15 @@
 function langRU() {;
 	/*translations itself*/
-	document.getElementById("avatar2").innerHTML = "Это мой аватар"
-	document.getElementById("menu1").innerHTML = "Мои творения"
-	document.getElementById("main").innerHTML = "Привет!"
+	document.getElementById("main").innerHTML = "Творения ivan123"
+	document.getElementById("nothing").innerHTML = "Здесь еще ничего нет :("
 	/*changing country flag and onclick function*/
 	document.getElementById("lang").src = "country-flags/uk.svg"
 	document.getElementById("lang").onclick = langEN;
 }
 function langEN() {;
 	/*translations itself*/
-	document.getElementById("avatar2").innerHTML = "This is my avatar."
-	document.getElementById("menu1").innerHTML = "My works"
-	document.getElementById("main").innerHTML = "Hello"
+	document.getElementById("main").innerHTML = "ivan123's creations"
+	document.getElementById("nothing").innerHTML = "Nothing here yet :("
 	/*changing country flag and onclick function*/
 	document.getElementById("lang").src = "country-flags/ru.svg"
 	document.getElementById("lang").onclick = langRU;
