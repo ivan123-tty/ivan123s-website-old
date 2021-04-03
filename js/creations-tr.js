34f05fc9c944e2c3bb0f599245f52f20a9ed44cb
ivan123 <70339505+ivan123gm@users.noreply.github.com>
2021-04-04 01:34:00 +0300

    small fix

--- js/creations-tr.js
+++ js/creations-tr.js
@@ -3,17 +3,19 @@ window.onload = () => {;
 
 function langRU() {;
 	/*translations itself*/
-	document.getElementById("creations").innerHTML = "Творения <br>ivan123"
-	document.getElementById("nothing").innerHTML = "Здесь еще ничего нет :("
+	document.getElementById("creations").innerHTML = "Творения <br>ivan123";
+	document.getElementById("menu1").innerHTML = "Обои, созданные мной.";
+	document.getElementById("all").innerHTML = "На этом пока все.";
 	/*changing country flag and onclick function*/
-	document.getElementById("lang").src = "country-flags/uk.svg"
+	document.getElementById("lang").src = "country-flags/uk.svg";
 	document.getElementById("lang").onclick = langEN;
 }
 function langEN() {;
 	/*translations itself*/
-	document.getElementById("creations").innerHTML = "Creations <br>of ivan123"
-	document.getElementById("nothing").innerHTML = "Nothing here yet :("
+	document.getElementById("creations").innerHTML = "Creations <br>of ivan123";
+	document.getElementById("menu1").innerHTML = "wallpapers, created by me.";
+	document.getElementById("all").innerHTML = "That's all for now.";
 	/*changing country flag and onclick function*/
-	document.getElementById("lang").src = "country-flags/ru.svg"
+	document.getElementById("lang").src = "country-flags/ru.svg";
 	document.getElementById("lang").onclick = langRU;
 }
