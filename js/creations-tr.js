a99f18a9fc01c0c74bff61be7fcf7dbac12e2526
ivan123 <70339505+ivan123gm@users.noreply.github.com>
2021-04-08 17:16:34 +0300

    fixes, fixes and fixes + adding new link to creations page

--- js/creations-tr.js
+++ js/creations-tr.js
@@ -5,6 +5,7 @@ function langRU() {;
 	/*translations itself*/
 	document.getElementById("creations").innerHTML = "Творения <br>ivan123";
 	document.getElementById("menu1").innerHTML = "Обои, созданные мной.";
+	document.getElementById("menu2").innerHTML = "Этот сайт";
 	document.getElementById("all").innerHTML = "На этом пока все.";
 	/*changing country flag and onclick function*/
 	document.getElementById("lang").src = "country-flags/uk.svg";
@@ -14,6 +15,7 @@ function langEN() {;
 	/*translations itself*/
 	document.getElementById("creations").innerHTML = "Creations <br>of ivan123";
 	document.getElementById("menu1").innerHTML = "wallpapers, created by me.";
+	document.getElementById("menu2").innerHTML = "this website";
 	document.getElementById("all").innerHTML = "That's all for now.";
 	/*changing country flag and onclick function*/
 	document.getElementById("lang").src = "country-flags/ru.svg";
