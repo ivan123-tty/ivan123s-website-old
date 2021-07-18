7a1a6f5bd6794c7a916fc4036c70863e87659267
ivan123 <70339505+ivan123gm@users.noreply.github.com>
2021-07-18 21:01:58 +0300

    fixing translation

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ window.onload = () => {;
 function langRU() {;
 	/*translations itself*/
 	document.getElementById("main").innerHTML = "Привет!"
+	document.getElementById("menu1").innerHTML = "Мои творения"
+	document.getElementById("menu2").innerHTML = "Обо мне"
 	/*changing country flag and onclick function*/
 	document.getElementById("lang").src = "country-flags/uk.svg"
 	document.getElementById("lang").onclick = langEN;
@@ -11,6 +13,8 @@ function langRU() {;
 function langEN() {;
 	/*translations itself*/
 	document.getElementById("main").innerHTML = "Hello!"
+	document.getElementById("menu1").innerHTML = "My creations"
+	document.getElementById("menu2").innerHTML = "About me"
 	/*changing country flag and onclick function*/
 	document.getElementById("lang").src = "country-flags/ru.svg"
 	document.getElementById("lang").onclick = langRU;
