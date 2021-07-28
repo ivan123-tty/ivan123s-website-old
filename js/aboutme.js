d78ad75449b3007fe9fadc7a5e77ebb517ee04da
ivan123 <70339505+ivan123gm@users.noreply.github.com>
2021-07-28 22:40:58 +0300

    fixes

--- js/aboutme.js
+++ js/aboutme.js
@@ -4,7 +4,7 @@ window.onload = () => {
 function langRU() {
 	/*translations itself*/
 	document.getElementById("main1").innerHTML = "Привет!"
-    document.getElementById("main2").innerHTML = "Я ivan123,<br> 15-летний подросток<br> интересующийся технологиями.<br> Живу в России."
+    document.getElementById("main2").innerHTML = "Я ivan123,<br> подросток интересующийся технологиями.<br> Живу в России."
     document.getElementById("main3").innerHTML = "Кстати, у меня есть Discord сервер.<br> Если хотите присоединиться к нему,<br>"
     document.getElementById("main4").innerHTML = "вот приглашение."
 	document.getElementById("main5").innerHTML = "Моя электронная почта:<br>ivan123another@gmail.com"
@@ -15,7 +15,7 @@ function langRU() {
 function langEN() {
 	/*translations itself*/
 	document.getElementById("main1").innerHTML = "Hello!"
-    document.getElementById("main2").innerHTML = "I'm ivan123,<br> a 15 years old teenager<br> interested in tech.<br> I'm living in Russia."
+    document.getElementById("main2").innerHTML = "I'm ivan123,<br> a teenager interested in tech.<br> I'm living in Russia."
     document.getElementById("main3").innerHTML = "Btw, i have a Discord server.<br> If you want to join it,<br>"
     document.getElementById("main4").innerHTML = "invite is here."
 	document.getElementById("main5").innerHTML = "My e-mail:<br>ivan123another@gmail.com"
