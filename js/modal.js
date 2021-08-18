fc73b9124ab2e84f768c1486fe8bf02eb21de263
ivan123 <70339505+ivan123gm@users.noreply.github.com>
2021-08-18 18:22:06 +0300

    now everything modal related is in one function!

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,14 @@
-// fixme: do everything modal related in one function
 function modal() {
 var modal = document.getElementById("modalBg");
-var close = document.getElementsByClassName("close");
+var close = document.getElementById("close");
 	modal.style.display = "block";
 
 window.onclick = function(event) {
 	if (event.target == modal) 
 	  modal.style.display = "none";
 	}
-}
 
-function closeModal() {
-var modal = document.getElementById("modalBg");
+close.onclick = function() {
 	modal.style.display = "none";
+	}
 }
